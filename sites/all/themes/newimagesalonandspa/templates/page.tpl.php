cc2660f907faf0188f1a73e55591ea71b7564ff3
Tyler <tmarshall@acromediainc.com>
2016-10-08 00:35:04 -0700

    Adds Services Grid with Hover Over Css changes
    Adds z-index on menu
    Adds non-opacity on links

--- sites/all/themes/newimagesalonandspa/templates/page.tpl.php
+++ sites/all/themes/newimagesalonandspa/templates/page.tpl.php
@@ -17,9 +17,9 @@
   </div>
 
   <div id="content">
-<!--    --><?php //print render($title_prefix); ?>
-<!--    --><?php //if ($title): ?><!--<h1>--><?php //print $title; ?><!--</h1>--><?php //endif; ?>
-<!--    --><?php //print render($title_suffix); ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
 
     <?php print render($messages); ?>
     <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
